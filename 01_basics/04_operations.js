1847fe32f8ccdcb85e981ff5a155748a241d4ba9
sakshi goyal <98320341+sakshigoyal-19@users.noreply.github.com>
2024-07-06 13:41:34 +0000

    operations

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,46 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+
+// console.log(2+2);     addition 
+// console.log(2-2);     substraction
+// console.log(2*2);     multiplication
+// console.log(2**3);    power
+// console.log(2/3);     division
+// console.log(2%3);     remainder
+
+let str1 = "hello"
+let str2 = " Sakshi"
+
+let str3 = str1 + str2
+console.log(str3);
+
+/* 
+
+// console.log("1" + 2);       //12
+// console.log(1 + "2");       //12
+// console.log("1" + 2 + 2);   //122
+// console.log(1 + 2 + "2");   //32 (if numeric value comes first they performs the desired operation)
+
+//logic behind above operations: if a string appears first, all other values are treated as strings, and if it appears last, all previous operations will be carried out.
+
+*/
+
+// console.log(+true); //not suggested
+// console.log(+"");   //not suggested
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2    //not an ideal way to define value to variable
+
+
+//postfix and prefix
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+++gameCounter;
+console.log(gameCounter);
+
+
+
+
