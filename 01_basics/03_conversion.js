bf516e80920e74189d4e6cef9b76e852485011c6
sakshi goyal <98320341+sakshigoyal-19@users.noreply.github.com>
2024-07-06 08:23:58 +0000

    conversion of datatype

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,20 @@
+let score = "Sakshi"
+console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+//"33" => 33  easy conversion
+//"33adf" => NaN (not an number)
+//true => 1; false=> 0  vise-versa
+
+
+let isLoggedIn = 1    //sakshi =>true
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
