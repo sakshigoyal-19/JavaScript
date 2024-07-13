3f780598dd0db836187484f6dfd64b04b5ad284d
sakshi goyal <98320341+sakshigoyal-19@users.noreply.github.com>
2024-07-13 16:03:08 +0000

    Functions

---
+++ 02_basics/04_functions.js
@@ -0,0 +1,16 @@
+function sayMyName(){
+    console.log("S");
+    console.log("A");
+    console.log("K");
+    console.log("S");
+    console.log("H");
+    console.log("I");
+}
+
+// sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(3,4)
