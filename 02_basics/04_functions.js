3a47c97f1e7ac79d3c2dab0620fc5dc00d95770b
sakshi goyal <98320341+sakshigoyal-19@users.noreply.github.com>
2024-07-14 08:38:43 +0000

    Functions

--- 02_basics/04_functions.js
+++ 02_basics/04_functions.js
@@ -7,10 +7,56 @@ function sayMyName(){
     console.log("I");
 }
 
-// sayMyName()
+// sayMyName()       //sayMyName -> reference, sayMyName()-> execution
 
-function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+// function addTwoNumbers(number1, number2){     //(number1, number2)->parameters
+//     console.log(number1 + number2);
+// }
+
+
+function addTwoNumbers(number1, number2){     
+    // let result = number1+number2
+    // return result
+    // console.log("sakshi");     //wont't be executed (rule in function-> anything written after return statement won't be executed)
+
+    return number1+number2
 }
 
-addTwoNumbers(3,4)
+const result = addTwoNumbers(3,4)   //(3,4)->arguments
+// console.log("Result: ", result);
+
+
+function loginUserMessage(username){
+    if(username === undefined){//(!username)
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage("Sakshi"));
+// console.log(loginUserMessage());      //returns undefined not null
+
+
+//shopping cart situation
+//rest operator -> ...(returns values in array)
+function calculateCartPrice(...num1){
+    return num1
+
+}
+console.log(calculateCartPrice(200,400,500));   
+
+const user ={
+    username: "sakshi",
+    price: 199
+}
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+// handleObject(user)
+
+const myNewArray = [200,300,500,800]
+function returnSecondValue(getArray){
+    return myNewArray[2]
+
+}
+console.log(returnSecondValue(myNewArray));
